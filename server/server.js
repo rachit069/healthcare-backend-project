e3d67d06a32c06e10030bb16f8e68663b14f824e
rachitmunjal <rachitmunjal69@gmail.com>
2024-10-21 15:13:32 +0530

    made some changes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// FRAMEWORK CONFIGURATION
 const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
@@ -9,3 +10,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(errorHandler);
+
+app.get('/',(req,res)=>{
+    res.send("error is still waiting");
+});
+
+// APP CONFIG START
+app.listen(port, () => {
+    console.log(`Server running on port https://localhost:${port}`);
+});
+
