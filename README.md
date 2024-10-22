7feebf7834c0ffb2095a5d770cb86189fa06bea5
rachitmunjal <rachitmunjal69@gmail.com>
2024-10-22 15:23:07 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # healthcare_project
-
--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 // FRAMEWORK CONFIGURATION
+// --- Always Import/Require on top --- 
 const express = require("express");
 const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middleware/errorHandler");
+
+// Handles error so that the code doesnt breakdown and crash.
+// Middlewares -> acts as a guard
+// const errorHandler = require("./middleware/errorHandler");
+
+// Cross-Origin Resource Sharing (CORS) is a browser security feature that allows a web page to access resources from a different domain than the one that served the page
 const cors = require("cors");
+
+// env file config
+const dotenv = require("dotenv");
+dotenv.config();
 
 connectDb();
 const app = express();
@@ -10,10 +20,10 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.get('/',(req,res)=>{
-    res.send("error is still waiting");
+    res.send("working");
 });
 
 // APP CONFIG START
