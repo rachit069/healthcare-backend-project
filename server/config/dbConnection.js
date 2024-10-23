b1df32a7ff8cdc952f6326355fce4c19c9caa89d
rachitmunjal <rachitmunjal69@gmail.com>
2024-10-24 00:53:30 +0530

    23-10-24

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -6,7 +6,7 @@ const connectDb = async() => {
     // try and catch basically hum error handling k liye use krre hain
     try{
         // await -> 
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.CONNECTIONSTRING);
         console.log("Database connected: ", connect.connection.host, connect.connection.name);
     }
     catch(err){
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const connectDb = require("./config/dbConnection");
 
 // Handles error so that the code doesnt breakdown and crash.
 // Middlewares -> acts as a guard
-// const errorHandler = require("./middleware/errorHandler");
+const errorHandler = require("./middlewares/errorHandler");
 
 // Cross-Origin Resource Sharing (CORS) is a browser security feature that allows a web page to access resources from a different domain than the one that served the page
 const cors = require("cors");
@@ -20,11 +20,32 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-// app.use(errorHandler);
+app.use(errorHandler);
+
+app.set('view engine' , 'hbs');
 
 app.get('/',(req,res)=>{
     res.send("working");
 });
+
+app.get("/home",(req,res) =>{
+    const user2 = {
+        "name": "rachit",
+        "age": "20",
+    }
+    const user1 = {
+        "name": "walia",
+        "age": "21" 
+    }
+    // let user = User.findOne({id:})
+    res.render("home",{user1, user2})
+})
+
+app.get("/allusers", (req, res) => {
+  // let user = User.findOne({id:})
+  res.render("users", { users:[{id:1, username:"rachit",age:23},{id:1,username:"walia",age:24}]});
+});
+
 
 // APP CONFIG START
 app.listen(port, () => {
